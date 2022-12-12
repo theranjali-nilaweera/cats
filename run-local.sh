deb693766cd0aba435031ccdc111ddc8ea22b66b
TN <theranjali@gmail.com>
2022-12-13 10:25:26 +1100

    Fixes to run script

--- run-local.sh
+++ run-local.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Change nvm"
 source ~/.bash_profile
-nvm use || { echo "Error changing node version"; exit 1; }
+nvm use || { echo "Error changing node version NVM not found"; exit 1; }
 
-echo "Starting up local movie API"
-npm run start || { echo "Error running local movie API"; exit 1; }
+echo "Starting up local cat API"
+npm run start || { echo "Error running local cat API"; exit 1; }
